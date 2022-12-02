d58066f337417bd7357e09009c474cf88f912d9b
Luca Cavalleri <luca.cavallery@gmail.com>
2022-12-02 17:13:33 +0100

    day1

--- 2022/day_01/main.py
+++ 2022/day_01/main.py
@@ -1,4 +1,19 @@
 
+def read():
+  y = []
+  current = []
+  with open('input.txt') as fp:
+    for line in fp:
+      line = line.strip()
+      if not line:
+        y.append(current)
+        current = []
+      else:
+        current.append(int(line))
+  return y
   
 def solve_1(input : list[list[int]]) -> int:
   return max(map(sum,input))
+
+def solve_2(input : list[list[int]], k=3) -> int:
+  return sum(sorted(map(sum,input),reverse=True)[:k])
